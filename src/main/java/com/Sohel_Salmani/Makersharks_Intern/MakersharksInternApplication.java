0fe5ed76726ac6dbbf77c75e311465108b004d50
real_sohel <salmanisohel26@gmail.com>
2024-08-21 15:21:29 +0530

    Initialized the project

---
+++ src/main/java/com/Sohel_Salmani/Makersharks_Intern/MakersharksInternApplication.java
@@ -0,0 +1,13 @@
+package com.Sohel_Salmani.Makersharks_Intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakersharksInternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakersharksInternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Sohel_Salmani/Makersharks_Intern/MakersharksInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sohel_Salmani.Makersharks_Intern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakersharksInternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
